0e89ec99c9e5dcdfaf4bff29bebbf4e82dc7c461
Rajee Karunaratne <rajee.karunaratne@gmail.com>
2020-07-08 18:54:17 -0400

    clipboardcopy-and-authorization

--- snippets/models.py
+++ snippets/models.py
@@ -3,7 +3,7 @@ from users.models import User
 
 # Create your models here.
 class Snippet(models.Model):
-    code = models.CharField(max_length=500)
+    code = models.TextField(max_length=500)
     language = models.CharField(max_length=255)    
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=500)   
